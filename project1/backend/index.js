5391d030deeda406885462fd85880e8e8193c7ca
pokorowski <pawelokorowski@gmail.com>
2020-04-22 23:54:22 +0200

    lcm project

---
+++ project1/backend/index.js
@@ -0,0 +1,83 @@
+const keys = require('./keys');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const redis = require('redis');
+
+
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+
+const { Pool } = require('pg');
+const pgClient = new Pool({ 
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort
+});
+
+pgClient.on('error', () => console.log('No connection to PG DB'));
+
+pgClient.query('CREATE TABLE IF NOT EXISTS results(number INT)').catch(err => console.log(err));
+
+console.log(keys)	
+ 
+app.get("/", (req, resp) => {
+    resp.send("Hello World!!!!");
+}); 
+
+app.get('/:number1/:number2', (req, resp) => {
+    let numberValue1 = req.params.number1;
+    let numberValue2 = req.params.number2;
+    let cache = numberValue1 + ',' + numberValue2;
+
+    redisClient.get(cache, (err, cachedValue) => {
+        if (!cachedValue) {
+          var lcmResult = parseInt(lcm(numberValue1, numberValue2))
+          redisClient.set(cache,lcmResult);
+          resp.send('[NO CACHE] '  + lcmResult + "\n");
+          pgClient.query('INSERT INTO results(number) VALUES($1)', [lcmResult], (err, res) => {
+            if(err){
+             console.log(err.stack);
+            };
+          })
+        }
+        else {
+            resp.send('[CACHE] ' + cachedValue + "\n");
+        };
+    });
+});
+
+app.get('/results', (req, resp) => {
+    pgClient.query('SELECT * FROM results')
+        .then(res => resp.send(res.rows))
+        .catch(err => console.log(err));
+});
+ 
+app.listen(4000, () => {
+  console.log('Server listening on port 4000');
+});
+
+function gcd(a,b) {
+  let temp;
+   while (b !== 0){
+     temp = a;
+      a = b;
+      b = temp % b;
+    }
+    return a;
+}
+
+function lcm(a,b) {
+  return a*b/gcd(a,b);
+}
+
+
---
+++ project1/frontend/src/App.js
@@ -0,0 +1,74 @@
+import React, {Component } from 'react'
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  
+  
+  constructor(props){
+	super(props)
+     this.state = {
+	   number1: 1,
+	   number2: 1,
+	   lcm: 0
+	  }
+	};
+
+  handleNumber1Change = (event) => {
+	this.setState({number1: event.target.value})
+  };
+	
+	
+  handleNumber2Change = (event) => {
+	this.setState({number2: event.target.value})
+  };
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+	let res = await axios.get(`api/${this.state.number1}/${this.state.number2}`);
+    this.state.lcm = res.data;
+    this.setState({lcm: this.state.lcm})
+    console.log(res);
+  };
+
+
+  render() {
+   return(
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+     	  <form onSubmit={this.handleSubmit}>
+			<div>
+			  <ul>
+				<li><label>Number 1</label>
+				<input
+				type="number"
+				min="1"
+				value={this.state.number1}
+				onChange={this.handleNumber1Change}
+				/>
+				</li>
+				<li><label>Number 2</label>
+				<input
+				type="number" 
+				min="1"
+				value={this.state.number2}
+				onChange={this.handleNumber2Change}
+				/>
+				</li>
+			  </ul>	
+			  <button type="submit">Count Least Common Multiple</button>
+			  <br/>
+			  Result: {this.state.lcm}	
+			</div>
+		</form>
+      </header>
+    </div>
+   )
+  }
+	
+
+}
+
+export default App;
